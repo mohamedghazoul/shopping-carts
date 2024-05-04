51ab481f92cc845c0de217719bfbb4df95a359cd
Mohamed ghazoul <126819472+mohamedghazoul@users.noreply.github.com>
2024-05-04 14:27:54 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,7 @@
 let tb = document.querySelector(".tbody")
-
+let name = document.querySelector(".name-login")
+let password = document.querySelector(".password-login")
+let submit = document.querySelector(".submit-login")
 
 
 // let data = [
@@ -9,28 +11,42 @@ let tb = document.querySelector(".tbody")
 // if(data.name !=localStorage.getItem("name")){
     // data.push({name:localStorage.getItem("name"),email:localStorage.getItem("email"),password:localStorage.getItem("password")})
     // }
-    if (localStorage.getItem("name")!= null){
+
+
+
+    // let name = localStorage.getItem("name")?JSON.parse(localStorage.getItem("name")):[]
+    // let password = localStorage.getItem("password")?JSON.parse(localStorage.getItem("password")):[]
+    // let email = localStorage.getItem("email")?JSON.parse(localStorage.getItem("email")):[]
         
-    let data = [];
-    data.push(localStorage)
-    data.map((item)=>{
-        tb.innerHTML += 
-        `<tr>
-        <td>${item.name}</td>
-        <td>${item.email}</td>
-        <td>${item.password}</td>
-        </tr>`
+submit.addEventListener("click",function(e){
+    e.preventDefault()
+    if(name.value=="mohamed"&&password.value=="mohamed2003"){
+        document.querySelector("form").style.opacity = "0"
+        document.querySelector("table").style.opacity = "1"
+    }
+})
+
+    let data = localStorage.getItem("data")?JSON.parse(localStorage.getItem("data")):[]
+
+
+    let x= data.map((item)=>{
+        return `
+        <tr>
+            <td scope="col">${item.name}</td>
+            <td scope="col">${item.email}</td>
+            <td scope="col">${item.password}</td>
+          </tr>
+        `
     })
-    console.log(data)
-} else {
-}
+    tb.innerHTML += x.join("")
+
 
 
 
 
 
 // console.log(data)
-console.log(localStorage)
+// console.log(localStorage)
 
 // setTimeout(() => {
     //     location.reload()
